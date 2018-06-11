d952982b4d8de6bfd3c64ed6e1b4453f003e85ba
KLPainter <KLPainter@gmail.com>
2018-06-11 15:42:55 -0700

    table scripts complete

--- server/db-client.js
+++ server/db-client.js
@@ -1,4 +1,5 @@
-const DATABASE_URL = 'postgres://localhost:5432/gallery';
+// const DATABASE_URL = 'postgres://localhost:5432/gallery';
+const DATABASE_URL = 'postgres://postgres:1234@localhost:5432/gallery';
 const pg = require('pg');
 const Client = pg.Client;
 
