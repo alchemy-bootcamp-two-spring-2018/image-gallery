66eec32cfd721cb861e962d13e9f6dd24d062281
Ryan Luras <ryanluras@gmail.com>
2018-06-11 14:52:05 -0700

    initial file structure

---
+++ app/src/components/Albums.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>This is the Album page yo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ app/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>I am the Home Component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
