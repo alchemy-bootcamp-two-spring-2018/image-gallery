ebdebfab24ffcb297fc3d57b17395204d650c933
rbtprograms <rbt61690@gmail.com>
2018-06-11 15:13:39 -0700

    routes done, create-tables made

--- app/src/router.js
+++ app/src/router.js
@@ -1,10 +1,12 @@
 import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
+import About from './components/About.vue';
 import Albums from './components/Albums.vue';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
+    { path: '/about', component: About },
     { path: '/albums', component: Albums },
   ]
 })
