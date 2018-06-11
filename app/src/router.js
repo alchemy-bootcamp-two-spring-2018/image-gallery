dc0a19f4f1d472fefd782ece892841574f1e54f5
Erik Withak <ErikAanestad@gmail.com>
2018-06-11 13:47:43 -0700

    builted the foundations;

---
+++ app/src/router.js
@@ -0,0 +1,27 @@
+import VueRouter from 'vue-router';
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import Albums from './components/Albums.vue';
+import Thumbnail from './components/Thumbnail.vue';
+import Gallery from './components/Gallery.vue';
+import List from './components/List.vue';
+import New from './components/New.vue';
+
+export default new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/albums', component: Albums },
+    { 
+      path: '/albums/:id',
+      children: [
+        { path: 'thumbnail', component: Thumbnail },
+        { path: 'gallery', component: Gallery },
+        { path: 'list', component: List },
+        { path: 'new', component: New },
+        { path: '', redirect: 'thumbnail' }
+      ]
+    },
+    { path: '*', redirect: '/' }
+  ]
+});
