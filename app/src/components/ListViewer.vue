c36bf5ffa88e3b18bb849a1035be536e057ef032
Mark Myers <markalope@gmail.com>
2018-06-12 16:56:58 -0700

    initial styling of list view

--- app/src/components/ListViewer.vue
+++ app/src/components/ListViewer.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="list-viewer">
     <h2>This is the List Viewer component</h2>
-    <ul v-if="images !== null">
-      <li
+    <div v-if="images !== null">
+      <div class ="list-image"
         v-for="image in images"
         :key="image.id"
       >
-        <img v-bind:src="image.url"/><br/>
-        <strong>{{ image.title }}</strong><br/>
+        <img v-bind:src="image.url" :title="image.title"/> 
+        <p><strong>{{ image.title }}</strong><br>
         {{ image.description }}
-      </li>
-    </ul>
+      </p>
+        </div>  
+        
+        
+      </div>
+    </div> 
   </div>
 </template>
 
@@ -33,6 +37,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+img {
+  float: left;
+  clear: both;
+  margin: 5px;
+  width: 50px;
+  height: 50px;
+  display: inline;
+}
+.list-image {
+  background-color: rgb(182, 247, 211);
+  text-align: left;
+}
+p {
+  font-size: 18px;
+  
+}
 
 </style>
