dce827599a37481ace2b10ccc090eb2c91896266
KLPainter <KLPainter@gmail.com>
2018-06-11 17:19:41 -0700

    'added sub nav'

--- app/src/components/AlbumDetail.vue
+++ app/src/components/AlbumDetail.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="album-detail">
     <h2>This is the Album Detail component</h2>
+      <nav>
+        <router-link to="list">List View</router-link>
+        &nbsp;
+        <router-link to="thumbnail">Thumbnail View</router-link>
+        &nbsp;
+        <router-link to="gallery">Gallery View</router-link>
+        &nbsp;
+        <router-link to="add-image">Add Image</router-link>
+      </nav>
+    <router-view />
   </div>
 </template>
 
