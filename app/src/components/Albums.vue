80446ee7afcc7b92be4a8ba6c17c7fb27e8ae4c0
Ryan Luras <ryanluras@gmail.com>
2018-06-11 17:09:52 -0700

    database working, api calls working

--- app/src/components/Albums.vue
+++ app/src/components/Albums.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
     <h2>This is the Album page yo</h2>
+    <ul v-if="albums">
+      <li v-for="album in albums"
+        :key="album.id"
+        >
+        {{ album.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { getAlbums } from '../services/api';
+
 export default {
+  data() {
+    return {
+      albums: null
+    };
+  },
+  created() {
+    getAlbums()
+      .then(albums => {
+        this.albums = albums;
+      });
+  }
 
 };
 </script>
