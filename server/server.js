b6fb9932d81974038f2e97f9153cf8f674dfda46
KLPainter <KLPainter@gmail.com>
2018-06-12 16:14:23 -0700

    can add images

--- server/server.js
+++ server/server.js
@@ -21,4 +21,36 @@ app.get('/api/albums', (req, res) => {
   });
 });
 
+app.get('/api/images/:id', (req, res) => {
+  client.query(`
+    SELECT id,
+    album_id,
+    title,
+    description,
+    url
+  FROM images
+  WHERE album_id = $1
+  ORDER BY title;
+  `,
+  [req.params.id]
+    ).then(result => {
+    res.send(result.rows);
+    });
+});
+
+// ROUTE:  Post to programs
+app.post('/api/images', (req, res) => {
+  const body = req.body;
+  client.query(`
+    INSERT INTO images (title, url, description, album_id)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+  `,
+  [body.title, body.url, body.description, body.albumId]
+  ).then(result => {
+      // send back object
+      res.send(result.rows[0]);
+    });
+});
+
 app.listen(3000, () => console.log('server running ...'));
