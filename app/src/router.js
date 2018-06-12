6600a87164bc22205fad9927401b92f3f87271d4
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-12 15:23:22 -0700

    Routed albums.vue links

--- app/src/router.js
+++ app/src/router.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router';
 import Home from './components/Home.vue';
 import Albums from './components/Albums.vue';
 import About from './components/About.vue';
+import AlbumDetail from './components/AlbumDetail.vue';
 
 export default new VueRouter({
   routes: [
