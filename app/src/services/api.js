43b6b6c2ba99c5f23879bd16573a550a4c3354c9
MariahAdams <mariah.adams23@yahoo.com>
2018-06-11 17:29:55 -0700

    added more scaffolding for files

---
+++ app/src/services/api.js
@@ -0,0 +1,33 @@
+const URL = 'http://localhost:3000/api';
+const ALBUMS_URL = `${URL}/albums`;
+//images url?
+
+//getAlbum
+export function getAlbum() {
+
+}
+
+//addAlbum
+export function addAlbum() {
+
+}
+
+//updateAlbum
+export function updateAlbum() {
+
+}
+
+//removeAlbum
+export function removeAlbum() {
+
+}
+
+//getImage
+export function getImage() {
+
+}
+
+//newImage
+export function newImage() {
+  
+}
