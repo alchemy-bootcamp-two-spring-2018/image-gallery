570c4b00754d980e2a930b3f8ffda4c9a08f2f93
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-12 16:10:13 -0700

    added image table updated scripts

--- server/scripts/drop-tables.js
+++ server/scripts/drop-tables.js
@@ -2,6 +2,8 @@ const client = require('../db-client');
 
 client.query(`
     DROP TABLE IF EXISTS albums;
+    DROP TABLE IF EXISTS images;
+    
 `)
 .then(
     () => console.log('drop tables complete'),
--- server/scripts/seed-data.js
+++ server/scripts/seed-data.js
@@ -1,5 +1,6 @@
 const client = require('../db-client');
 const albums = require('../data/albums.json');
+const images = require('../data/images.json');
 
 Promise.all(
   albums.map(albums => {
@@ -8,6 +9,14 @@ Promise.all(
     VALUES ($1, $2);
     `,
     [albums.title, albums.description]
+    ).then(result => result.rows[0]);
+  }),
+  images.map(images => {
+    return client.query(`
+    INSERT INTO images (album_id, title, description, url)
+    VALUES ($1, $2, $3, $4);
+    `,
+    [images.album_id, images.title, images.description, images.url]
     ).then(result => result.rows[0]);
   })
 )
