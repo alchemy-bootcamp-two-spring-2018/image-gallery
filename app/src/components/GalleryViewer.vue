dc946985cb836deabc2b80152240522efec2bac6
Mark Myers <markalope@gmail.com>
2018-06-13 09:56:48 -0700

    plugged in gallery component

--- app/src/components/GalleryViewer.vue
+++ app/src/components/GalleryViewer.vue
@@ -1,15 +1,35 @@
-<template>
-  <div id="gallery-viewer">
+ <template>
+  <div>
     <h2>This is the Gallery Viewer component</h2>
+    <gallery :images="urlList" :index="index" @close="index = null"></gallery>
+    <div
+      class="image"
+      v-for="(image, imageIndex) in urlList"
+      :key="imageIndex"
+      @click="index = imageIndex"
+      :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
+    ></div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+  import VueGallery from 'vue-gallery';
+  
+  export default {
+    data: function () {
+      return {
+      
+        index: null
+      };
+    },
+    props: ['images'],
+    computed: {
+      urlList() {
+        return this.images.map(image => image.url);
+      },
+    },
+    components: {
+      'gallery': VueGallery
+    },
+  }
+</script> 
