ef8e48a06372f3589fa8f06723cc86788b0e95e8
Mark Myers <markalope@gmail.com>
2018-06-14 14:32:39 -0700

    added dot env files and rules

--- server/scripts/drop-tables.js
+++ server/scripts/drop-tables.js
@@ -4,10 +4,10 @@ client.query(`
     DROP TABLE IF EXISTS images;
     DROP TABLE IF EXISTS albums;
     `)
-    .then(
-        () => console.log('drop tables complete'),
-        err => console.log(err)
-    )
-    .then(() => {
-        client.end();
-    });
+  .then(
+    () => console.log('drop tables complete'),
+    err => console.log(err)
+  )
+  .then(() => {
+    client.end();
+  });
