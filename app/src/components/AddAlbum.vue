dbd318422420d5e466385e5aa407db35af0d4454
Easton Gorishek <ieaston26@gmail.com>
2018-06-14 16:20:47 -0700

    add album functionality working

--- app/src/components/AddAlbum.vue
+++ app/src/components/AddAlbum.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
     <h1>Add Album</h1>
-
     
+    <form @submit.prevent="handleSubmit">
+      <input type="text" placeholder="Album Name" v-model="newAlbum.decade">
+      &nbsp;
 
+      <input type="text" placeholder="Description" v-model="newAlbum.description">
+      &nbsp;
+
+      <button type="submit">Submit</button>
+    </form>
+    
   </div>
 </template>
 
 <script>
 export default {
+
+  data() {
+    return {
+      newAlbum: {
+        decade: null,
+        description: null
+      }
+    };
+  },
+
+  props: ['onPost'],
+
+  methods: {
+    handleSubmit() {
+      this.onPost(this.newAlbum);
+    }
+  },
 
 }
 </script>
